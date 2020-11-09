54a4b890309b82e363e4d6796fc6f3fb3e652461
eren.terlikli <44238447+erenterlikli@users.noreply.github.com>
2020-11-09 17:47:25 +0300

    Add files via upload

---
+++ Grafikler/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Grafikler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.Değer:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(13, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2.Değer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(12, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3.Değer:";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(104, 45);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(127, 20);
+            this.txt1.TabIndex = 3;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(104, 89);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(127, 20);
+            this.txt2.TabIndex = 4;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(104, 127);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(127, 20);
+            this.txt3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(104, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(104, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sıfırla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(652, 313);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Grafik İşlemleri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Grafikler/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafikler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float d1, d2, d3, toplam; //değerleri almak
+            d1 = int.Parse(txt1.Text);
+            d2 = int.Parse(txt2.Text);
+            d3 = int.Parse(txt3.Text);
+
+            toplam = d1 + d2 + d3;
+
+            float pd1, pd2, pd3; //değerleri 360 derecelik grafik üzerinde yazdırmak
+            pd1 = (d1 / toplam) * 360;
+            pd2 = (d2 / toplam) * 360;
+            pd3 = (d3 / toplam) * 360;
+
+
+            Pen p = new Pen(Color.White, 4); //sınırları çizdirelim.
+            Graphics g = this.CreateGraphics();
+            Rectangle rec = new Rectangle(txt1.Location.X + txt1.Size.Width + 10, 10, 250, 250); //forma grafik için alan ayırdık.
+            Brush b1 = new SolidBrush(Color.Green); //1.alanın rengi
+            Brush b2 = new SolidBrush(Color.Yellow); //2.alanın rengi
+            Brush b3 = new SolidBrush(Color.Red); //3.alanın rengi
+
+            g.Clear(Form1.DefaultBackColor);
+
+            g.DrawPie(p, rec, 0, pd1); //1.alanı çiz ve içini doldur. Sınırları belli.
+            g.FillPie(b1, rec, 0, pd1);
+
+            g.DrawPie(p, rec, pd1, pd2); //2.alanı çiz ve içini doldur.
+            g.FillPie(b2, rec, pd1, pd2);
+
+            g.DrawPie(p, rec, pd1 + pd2, pd3); //3.alan için.
+            g.FillPie(b3, rec, pd1 + pd2, pd3);
+     
+
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt1.Clear();
+            txt2.Clear();
+            txt3.Clear();
+        }
+    }
+}
